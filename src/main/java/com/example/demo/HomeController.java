bee6b6199b6bc659aa7b63fc09580c6564859dcc
sunnycal <milana.campbell@gmail.com>
2019-05-14 14:59:09 -0400

    first commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,32 @@
-package com.example.demo;
+    package com.example.demo;
 
-//import class (class is the Controller, etc.)
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+    //import class (class is the Controller, etc.)
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
+    @Controller
+    //class
+    public class HomeController {
 
-@Controller
-//class
-public class HomeController {
+//        @RequestMapping("/")
+//        //parameter or controller method
+//        public String homePage(Model model){
+//            model.addAttribute("myvar", "Say hello to the people.");
+//            return "hometemplate";
+//        }
 
-    @RequestMapping("/")
-    //parameter
-    public String homePage(Model model){
-        model.addAttribute("myvar", "Say hello to the people.");
-        return "hometemplate";
+        @RequestMapping("/")
+        //method and can be called anything
+        public String myGreetings(Model model){
+            model.addAttribute("myvar", "hello, again");
+            model.addAttribute("myvar2", "i'm back!");
+            return "secondhometemplate";
+        }
+//
+//        @RequestMapping ("/")
+//        public String myComeback(Model model){
+//            model.addAttribute("myvar", "i'm back!");
+//            return "secondhometemplate";
+//        }
     }
-}
